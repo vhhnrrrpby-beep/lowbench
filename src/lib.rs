dc7ecbdaefee997c8d64b37b0da84999f2d249bb
Jotaro Nagamine <qbgbp56kub@icloud.com>
2026-09-05 13:21:04 +0900

    0.2.0パブリッシュ前

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
-#![cfg_attr(not(feature = "std"),no_std)]
+#![cfg_attr(not(feature = "std"), no_std)]
 use core::hint::black_box;
 use core::time::Duration;
 #[cfg(test)]
-#[cfg(feature = "std")]
 mod test;
 
 /// If using `no_std`, you need to provide a type that implements the following trait.
 /// This trait exists to signal timers in your environment.
 pub trait Timer {
-    fn now() -> Self ;
+    fn now() -> Self;
     fn elapsed(&self) -> Duration;
 }
 
@@ -30,73 +29,72 @@ impl Timer for Instant {
 /// measures the function three times.
 /// The arguments for this function change depending on whether `std` or `no_std` is used.
 #[cfg(feature = "std")]
-pub fn bench(f:impl FnMut()) -> [Duration;3] {
+pub fn bench(f: impl FnMut()) -> [Duration; 3] {
     let all_start = Instant::now();
     println!("\nBenchmark Running...");
-    let (result,count) =bench_inner::<Instant>(f);
+    let (result, count) = bench_inner::<Instant>(f);
     println!("Benchmark Result");
-    println!("averages  :: {:?}",result);
+    println!("averages  :: {:?}", result);
     println!("attempts  :: {count}");
-    println!("Running time  ::  {:?}\n",all_start.elapsed());
+    println!("Running time  ::  {:?}\n", all_start.elapsed());
     result
 }
 
-#[cfg(not(feature= "std"))]
-pub fn bench<T:Timer>(f:impl FnMut()) -> [Duration;3] {
+#[cfg(not(feature = "std"))]
+pub fn bench<T: Timer>(f: impl FnMut()) -> [Duration; 3] {
     (bench_inner::<T>(f)).0
 }
 
-const MAX_WARMUP_ATTEMPT:u8 = 10;
+const MAX_WARMUP_ATTEMPT: u8 = 10;
 const WARMUP_COUNTS: [usize; 3] = [100, 1000, 10000];
-fn bench_inner<T:Timer>(mut f:impl FnMut())  -> ([Duration;3],usize){
+fn bench_inner<T: Timer>(mut f: impl FnMut()) -> ([Duration; 3], usize) {
     let mut warmup_count = 0;
-    let mut tests:[Duration;2] = [Duration::new(0,0),Duration::new(0,0)];
+    let mut tests: [Duration; 2] = [Duration::new(0, 0), Duration::new(0, 0)];
     for i in 0..2 {
         let start = T::now();
         for _ in 0..(WARMUP_COUNTS[warmup_count]) {
             black_box(f());
         }
         let elapsed = start.elapsed();
-        tests[i] = elapsed/(WARMUP_COUNTS[warmup_count] as u32);
+        tests[i] = elapsed / (WARMUP_COUNTS[warmup_count] as u32);
         if elapsed.as_nanos() < Duration::from_micros(10).as_nanos() {
             warmup_count += 1;
         };
     }
 
     let mut warmup_attempt = 0;
-    while tests[0].as_nanos().abs_diff(tests[1].as_nanos()) > Duration::new(0,100).as_nanos()
-    {
+    while tests[0].as_nanos().abs_diff(tests[1].as_nanos()) > Duration::new(0, 100).as_nanos() {
         let start = T::now();
         for _ in 0..(WARMUP_COUNTS[warmup_count]) {
             black_box(f());
         }
         let elapsed = start.elapsed();
         tests[0] = tests[1];
-        tests[1] = elapsed/(WARMUP_COUNTS[warmup_count] as u32);
-        warmup_attempt += 1 ;
+        tests[1] = elapsed / (WARMUP_COUNTS[warmup_count] as u32);
+        warmup_attempt += 1;
         if warmup_attempt > MAX_WARMUP_ATTEMPT {
             break;
         }
     }
-    
+
     let count = set_count(tests[1]);
 
-    let mut results:[Duration;3] = [Duration::new(0,0);3];
+    let mut results: [Duration; 3] = [Duration::new(0, 0); 3];
 
     for i in 0..3 {
         let start = T::now();
         for _ in 0..count {
             black_box(f());
-        };
+        }
         let elapsed = start.elapsed();
         results[i] = elapsed;
     }
-    (results.map(|x| x/(count as u32)),count)
+    (results.map(|x| x / (count as u32)), count)
 }
 
-const MAX_ATTEMPT:usize = 100_000_000;
-const MIN_ATTEMPT:usize = 100;
-fn set_count(tests:Duration) -> usize {
+const MAX_ATTEMPT: usize = 100_000_000;
+const MIN_ATTEMPT: usize = 100;
+fn set_count(tests: Duration) -> usize {
     if tests.as_nanos() == 0 {
         return MAX_ATTEMPT;
     }
@@ -106,5 +104,7 @@ fn set_count(tests:Duration) -> usize {
         MIN_ATTEMPT
     } else if count > MAX_ATTEMPT {
         MAX_ATTEMPT
-    } else {count}
+    } else {
+        count
+    }
 }
--- src/test.rs
+++ src/test.rs
@@ -1,21 +1,35 @@
 use super::*;
+extern crate std;
 
-fn add(a:usize,b:i32) -> i32 {
-    (a as i32)+b
+use core::time::Duration;
+use std::println;
+use std::time::Instant;
+
+#[cfg(not(feature = "std"))]
+impl Timer for Instant {
+    fn now() -> Self {
+        Instant::now()
+    }
+    fn elapsed(&self) -> Duration {
+        Instant::elapsed(self)
+    }
 }
 
-fn fib() { 
-    let _ = 1+1;
+fn add(a: usize, b: i32) -> i32 {
+    (a as i32) + b
 }
 
+fn fib() {
+    let _ = 1 + 1;
+}
 
-
-#[test] 
+#[test]
+#[cfg(feature = "std")]
 fn bench_test() {
     println!("first test \n");
     bench(|| {
-        for (i,x) in (3..104).enumerate() {
-            add(i,x);
+        for (i, x) in (3..104).enumerate() {
+            add(i, x);
         }
     });
 
@@ -26,4 +40,29 @@ fn bench_test() {
 
     println!("third test \n");
     bench(|| {});
+}
+
+#[test]
+#[cfg(not(feature = "std"))]
+fn bench_test() {
+    println!("first test \n");
+    println!(
+        "{:?}",
+        bench::<Instant>(|| {
+            for (i, x) in (3..104).enumerate() {
+                add(i, x);
+            }
+        })
+    );
+
+    println!("second test \n");
+    println!(
+        "{:?}",
+        bench::<Instant>(|| {
+            fib();
+        })
+    );
+
+    println!("third test \n");
+    println!("{:?}", bench::<Instant>(|| {}));
 }
